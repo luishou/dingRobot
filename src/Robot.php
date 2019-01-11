200d7f5dc425951c2bc40b947e065fc262059ed6
housulei <housulei@yazuo.com>
2019-01-11 14:06:06 +0800

    初始化demo

---
+++ src/Robot.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luis hou
+ * Date: 2019/1/11
+ * Time: 13:51
+ */
+
+namespace Luishou\Ding;
+
+
+class Robot
+{
+    public function desc()
+    {
+        echo "this is a test info";
+    }
+}
